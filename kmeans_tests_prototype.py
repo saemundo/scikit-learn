710e49f3acd0fbe21855650cd0e7348fb973a9d6
saemundo <soh@cs.stir.ac.uk>
2016-11-09 20:43:42 +0000

    Add files via upload

--- kmeans_tests_prototype.py
+++ kmeans_tests_prototype.py
@@ -4,8 +4,11 @@ import imp
 import inspect
 mod_name = 'test_k_means'
 mod_path = '/home/soh/allmighty_coding/scikit-learn/sklearn/cluster/tests/test_k_means.py'
-try_mod = imp.load_source(mod_name,mod_path)
-try_mod.gather_tests_and_run_as_one()
+try:
+    try_mod = imp.load_source(mod_name,mod_path)
+    try_mod.gather_tests_and_run_as_one()
+except Exception:
+    print {'name':'Kmeans_all','failures':0,'errors':478,'testsRun':478}
 # mod_funcs = dict(inspect.getmembers(try_mod,inspect.isfunction))
 # for key,val in mod_funcs.items():
 #     if key.startswith('test_'):
